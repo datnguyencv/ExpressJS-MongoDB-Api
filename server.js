c62929605d70faf1c5bc13926b52541bb7610ff4
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-11-16 17:17:25 +0700

    update backend fix url frontend

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const FRONTEND = process.env.FRONTEND
 app.use(express.json());
 var corsOptions = {
     origin: FRONTEND,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200 
 }
 
 app.use(cors(corsOptions))
